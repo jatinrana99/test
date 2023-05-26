07b1651b92ce24b6c3a7d90340241956df008328
Jatin rana <engr.jatinrana@gmail.com>
2023-05-26 17:03:48 +0530

    Created Test.vue Component

---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Test Component</div>
+    <section>
+        <div>Enter Something</div>
+        <input type="text">
+    </section>
+</template>
+
+<script>
+export default {
+    name:`Test`
+}
+</script>
