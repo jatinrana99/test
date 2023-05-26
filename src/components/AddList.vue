09465fce9e58380485fb9545d3e972bb06b4a953
Jatin rana <engr.jatinrana@gmail.com>
2023-05-26 18:50:46 +0530

    Data is successfully from FORM to list and then in app component

---
+++ src/components/AddList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <FormAdd v-on:submit-item="submit"/>
+        <ul>
+            <li v-for="(item,index) in items" v-bind:key="index">
+            <span>{{ item }}</span>
+            <div>
+                <i class="fa-solid fa-pen-to-square" v-on:click.prevent="editItem(index)"></i>
+                <i class="fa-solid trash" v-on:click.prevent="deleteItem(index)"></i>
+            </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import FormAdd from './FormAdd.vue'
+
+export default {
+    name:`AddList`,
+    components:{
+        FormAdd
+    },
+    data(){
+        return{
+            items:[]
+        }
+    },
+    methods:{
+        submit:function(name){
+            this.items.push(name);
+        },
+    //     editItem: function(index) {
+    //         this.editIndex=index;
+    //         this.name=this.items[index];
+    // },
+    deleteItem: function(index) {
+        this.items.splice(index,1);
+    }
+    }
+  }
+
+</script>
---
+++ src/components/FormAdd.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>Form
+    <p>Enter Something</p>
+        <i>Press enter</i>
+        <br/>
+        <form v-on:submit.prevent>
+            <input v-on:keypress="submit" type="text" v-model="name" />
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name:`AddForm`,
+    data(){
+        return{
+            name: " ",
+            editIndex:false
+        }
+    },
+    methods:{
+        submit: function(e) {
+      if (e.keyCode === 13) {
+        this.$emit("submit-item",this.name);
+        this.name="";
+      }
+        // if(this.editIndex){
+        //     this.items.splice(this.editIndex,1,this.name);
+        //     this.name="";
+        //     this.editIndex-1;
+        // }
+        // else{
+        //     this.items.push(this.name);
+        // this.name = "";
+        // }
+     
+      }
+       }
+    }
+
+</script>
