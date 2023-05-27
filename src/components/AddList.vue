0ce0271185ad68604b414020ac65c66905126416
Jatin rana <engr.jatinrana@gmail.com>
2023-05-27 14:17:32 +0530

    edit button is working properly

--- src/components/AddList.vue
+++ src/components/AddList.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <FormAdd v-on:submit-item="submit"/>
+        <!-- <FormAdd v-on:submit-item="submit"/> -->
+        <FormAdd v-bind:edit-index="editIndex" 
+                 v-bind:name="name"
+                 v-on:edit-item="edit"
+                 v-on:submit-item="submit">
+         </FormAdd>
         <ul>
-            <li v-for="(item,index) in items" v-bind:key="index">
+            <li v-for="(item,index) in data" v-bind:key="index">
             <span>{{ item }}</span>
             <div>
-                <!-- <i class="fa-solid fa-pen-to-square" v-on:click.prevent="editItem(index)"></i> -->
+                <i class="fa-solid fa-pen-to-square" v-on:click.prevent="editItem(index)"></i>
                 <i class="fa-solid fa-trash" v-on:click.prevent="deleteItem(index)"></i>
             </div>
             </li>
@@ -15,6 +20,7 @@
 
 <script>
 import FormAdd from './FormAdd.vue'
+// import Vue from 'vue'
 
 export default {
     name:`AddList`,
@@ -23,20 +29,34 @@ export default {
     },
     data(){
         return{
-            items:[]
+            data:[],
+            name:"",
+            editIndex:-1
         }
     },
     methods:{
         submit:function(name){
-            this.items.push(name);
+            this.data.push(name);
+            this.name="";
         },
-    //     editItem: function(index) {
-    //         this.editIndex=index;
-    //         this.name=this.items[index];
-    // },
     deleteItem: function(index) {
-        this.items.splice(index,1);
-    }
+        this.data.splice(index,1);
+    },
+        edit:function(obj){
+            var{name,editIndex} = obj;
+            // eslist disable the next Line
+            console.log("hello from edit");
+            console.log("actual edit", name , editIndex);
+            // Vue.set(this.data,editIndex, name);
+            this.data.splice(editIndex,1,name);
+            this.name="";
+            this.editIndex =-1;
+        },
+        editItem:function(index){
+            console.log("edit index");
+            this.editIndex= index;
+            this.name = this.data[index];
+        }
     }
   }
 
--- src/components/FormAdd.vue
+++ src/components/FormAdd.vue
@@ -4,7 +4,7 @@
         <i>Press enter</i>
         <br/>
         <form v-on:submit.prevent>
-            <input v-on:keypress="submit" type="text" v-model="name" />
+            <input v-on:keypress="submit" type="text" v-model="text" />
         </form>
     </div>
 </template>
@@ -14,15 +14,51 @@ export default {
     name:`AddForm`,
     data(){
         return{
-            name: " ",
-            editIndex:false
+            text:""
         }
     },
+
+    // adding watchers
+        watch: {
+            name: function(newName){
+                console.warn("watch ", newName);
+                this.text = newName;
+            }
+        },
+
+        props:{
+            name:{
+                type:String,
+                require:true
+            },
+            editIndex:{
+                type: Number,
+                require:true
+            }
+        },
+
     methods:{
         submit: function(e) {
       if (e.keyCode === 13) {
-        this.$emit("submit-item",this.name);
-        this.name="";
+        // this.$emit("submit-item",this.name);
+        // this.name="";
+      console.log(this.editIndex);
+      
+        
+        if(this.editIndex !== -1){
+
+            // eslist-disable the next line
+            console.warn("edit item");
+            this.$emit("edit-item",{
+                name:this.text,
+                editIndex: this.editIndex
+            });
+        }
+
+        else{
+            this.$emit("submit-item", this.text);
+        }
+        this.text="";
       }
         // if(this.editIndex){
         //     this.items.splice(this.editIndex,1,this.name);
